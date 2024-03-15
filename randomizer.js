e3f55fa01985097f98187be0ae647618e3d6572a
lukehschool <163425901+lukehschool@users.noreply.github.com>
2024-03-15 07:14:35 -0600

    Fixed html typos, added dummy function to button

---
+++ randomizer.js
@@ -0,0 +1,9 @@
+const playerBoards = [ "Deserts", "Plains", "Swamps", "Lakes", "Forests", "Mountains", "Wastelands" ];
+const factions = ["Blessed", "Felines", "Goblins", "Illusionists", "Inventors",
+"Lizards", "Moles", "Monks", "Navigators", "Omar", "Philosophers", "Psychics"];
+const palaces = Array.from({length: 16}, (_, i) => `Palace ${i + 1}`);
+
+
+function startRandomization() {
+    console.log( palaces);
+    }

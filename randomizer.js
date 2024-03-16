9cf56f30bd63c324f5a4dd081ca1fe7ea43ce8f3
lukehschool <163425901+lukehschool@users.noreply.github.com>
2024-03-16 06:44:31 -0600

    Randomizer and firstoutput set

--- randomizer.js
+++ randomizer.js
@@ -9,7 +9,28 @@ const competency = Array.from({length: 12}, (_, i) => `Competency Tile ${i + 1} 
 const roundScoring = Array.from({length: 12}, (_, i) => `Round Scoring Tile ${i + 1} `); // an array with numbered object for the 12 Round Scoring Tiles
 const endScoring = Array.from({length: 4}, (_, i) => `End Scoring Tile ${i + 1} `); // an array with numbered object for the  4 Endgame Scoring Tiles
 
+var minPlayers = 1;
+var maxPlayers = 5;
+
 function startRandomization() {
-    console.log( palaces);
-    console.log(innovations);
+    const playerCount = parseInt(document.getElementById('playerCount').value);
+    if (isNaN(playerCount) || playerCount < minPlayers || playerCount > maxPlayers){
+        alert('Please under a valid number of player between ' + minPlayers + ' and ' + maxPlayers +'.' );  // gives an alert if the number is out of range or not a number
+        return;
+        }
+        displayItems('playerBoards',shuffleTile(playerBoards.slice(0, playerCount +1 )));
+        displayItems('factions', shuffleTile(factions.slice(0, playerCount + 1 )));
     }
+    const display = document.getElementById('display');
+
+function shuffleTile(tile){
+    return tile.sort(() => Math.random() - 0.5);
+}
+
+function displayItems(title, items) {
+    const display = document.getElementById('display');
+    const section = document.createElement('div');
+    section.innerHTML = `<h3>${title}</h3><p>${items.join(', ')}</p>`;
+    display.appendChild(section);
+}
+

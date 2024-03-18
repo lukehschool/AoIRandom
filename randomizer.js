0ea27cc71a636f28cfa9b52b70a8238147334734
lukehschool <163425901+lukehschool@users.noreply.github.com>
2024-03-17 22:35:46 -0600

    Fixed min players and typo based on feedback

--- randomizer.js
+++ randomizer.js
@@ -3,7 +3,7 @@ const factions = ["Blessed", "Felines", "Goblins", "Illusionists", "Inventors",
 "Lizards", "Moles", "Monks", "Navigators", "Omar", "Philosophers", "Psychics"];
 const innovations = Array.from({length: 18}, (_, i) => `Innovation ${i + 1} `); // an array with numbered object for the 18 Innovation tiles
 const palaces = Array.from({length: 16}, (_, i) => `Palace ${i + 1}`);  // makes an array with an object for each numbered Palace, excluding the Special Palace which is either always or never included
-const roundBonus = Array.from({length: 10}, (_, i) => `Round Bounus Tile ${i + 1} `); // an array with numbered object for the  10 Round Bonus Tiles
+const roundBonus = Array.from({length: 10}, (_, i) => `Round Bonus Tile ${i + 1} `); // an array with numbered object for the  10 Round Bonus Tiles
 const bookActions = Array.from({length: 6}, (_, i) => `Book Action ${i + 1} `); // an array with numbered object for the 6 Book Aciton Tiles
 const competency = Array.from({length: 12}, (_, i) => `Competency Tile ${i + 1} `); // an array with numbered object for the  12 different Competency Tiles
 const roundScoring = Array.from({length: 12}, (_, i) => `Round Scoring Tile ${i + 1} `); // an array with numbered object for the 12 Round Scoring Tiles
